a7252fda74bb35d67d72a9a1d6bfbf4d60ac7061
Brayan Manzano <al221610173@gmail.com>
2025-03-10 17:57:59 -0600

    delete

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -94,6 +94,7 @@ class CategoryController extends Controller
      */
     public function destroy($id)
     {
-        //
+        Category::destroy($id);
+        return redirect()->route('categories.index')->with('success', 'Categoría eliminada correctamente.');
     }
 }
